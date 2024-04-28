d22c130cea6452db0b0ffe28ddff05012a2375fe
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-04-28 14:40:23 +0900

    feat: project-init

---
+++ src/main/kotlin/com/example/paymentsystemkotlin/PaymentSystemKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.paymentsystemkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentSystemKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentSystemKotlinApplication>(*args)
+}
