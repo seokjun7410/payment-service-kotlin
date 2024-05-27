5ddc4d1b18021c20639a462a63491132e59bd6ac
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-05-27 15:44:13 +0900

    feat : TossWebClient timeout 설정

--- src/main/kotlin/com/example/paymentsystemkotlin/payment/adpater/out/web/toss/TossWebClientConfiguration.kt
+++ src/main/kotlin/com/example/paymentsystemkotlin/payment/adpater/out/web/toss/TossWebClientConfiguration.kt
@@ -1,5 +1,6 @@
 package com.example.paymentsystemkotlin.payment.adpater.out.web.toss
 
+import io.netty.handler.timeout.ReadTimeoutHandler
 import org.apache.hc.core5.http.HttpHeaders
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 import reactor.netty.resources.ConnectionProvider
 import java.util.Base64
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class TossWebClientConfiguration(
@@ -30,7 +32,14 @@ class TossWebClientConfiguration(
     }
 
     private fun reactorClientHttpConnector(): ClientHttpConnector {
-        val provider = ConnectionProvider.builder("toss-payment").build()
-        return ReactorClientHttpConnector(HttpClient.create(provider))
+        val provider = ConnectionProvider.builder("toss-payment")
+                .build()
+
+        val clientBase = HttpClient.create(provider)
+                .doOnConnected {
+                    it.addHandlerLast(ReadTimeoutHandler(30, TimeUnit.SECONDS))
+                }
+
+        return ReactorClientHttpConnector(clientBase)
     }
 }
